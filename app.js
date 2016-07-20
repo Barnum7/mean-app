d9444ce00a0c2efc2e1ab8d4a272594f2de03347
Barnum <bgroat@apigarage.com>
2016-07-20 10:41:14 -0400

    Created  public/ to house static resources, and began exploring creating routes to return files, and JSON data

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 var express = require ( 'express' );
 var app = express();
+var path = require ( 'path' );
 
 app.set( 'port', 3000 );
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+app.get( '/', function( req, res ) {
+  console.log( 'GET the homepage' );
+  res
+    .status( 200 )
+    .sendFile( path.join( __dirname , 'public', 'index.html' ) );
+});
+
+app.get( '/json', function( req, res ) {
+  console.log( 'GET JSON' );
+  res
+    .status( 200 )
+    .json( {
+      'Asimov' : 'online'
+    } );
+});
+
+app.get( '/file', function( req, res ) {
+  console.log( 'GET File' );
+  res
+    .status( 200 )
+    .sendFile( path.join( __dirname , 'app.js' ) );
+});
 
 var server = app.listen( app.get( 'port' ), function() {
   var port = server.address().port;
